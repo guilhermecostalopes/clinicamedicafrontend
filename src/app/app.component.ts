db63c3f04a83f7dcf4ceff7f971179019d364274
gitlab <gitlab@netsoft.eti.br>
2020-03-28 17:44:26 -0300

    inclusão de translation - i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
   selector: "app-root",
@@ -7,4 +8,12 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   title = "Sistema de clinica médica";
+  public translate : any;
+  constructor(translate: TranslateService) {
+    translate.addLangs(["pt", "en"]);
+    translate.setDefaultLang("pt");
+
+    const browserLang = translate.getBrowserLang();
+    translate.use(browserLang.match(/pt|en/) ? browserLang : "pt");
+  }
 }
--- src/app/core/comum.module.ts
+++ src/app/core/comum.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from "@angular/core";
 import { MaterialModule } from "./material-module/material.module";
 import { CommonModule } from "@angular/common";
-import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import {
+  HttpClientModule,
+  HTTP_INTERCEPTORS,
+  HttpClient
+} from "@angular/common/http";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { AlertModule } from "ngx-alerts";
 import { DialogComponent } from "./dialog/dialog.component";
 import { LoaderService } from "./services/loader/loader.service";
 import { AuthInterceptor } from "../security/interceptor/auth.interceptor";
+import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   imports: [
@@ -15,7 +25,14 @@ import { AuthInterceptor } from "../security/interceptor/auth.interceptor";
     FormsModule,
     ReactiveFormsModule,
     AlertModule.forRoot({ maxMessages: 5, timeout: 5000, position: "right" }),
-    HttpClientModule
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   declarations: [DialogComponent],
   providers: [
@@ -34,7 +51,8 @@ import { AuthInterceptor } from "../security/interceptor/auth.interceptor";
     ReactiveFormsModule,
     AlertModule,
     DialogComponent,
-    HttpClientModule
+    HttpClientModule,
+    TranslateModule
   ]
 })
 export class ComumModule {}
