afa0987020eda9f1fdb88980ae41870c0aef753f
gitlab <guilherme@netsoft.eti.br>
2020-04-06 11:19:42 -0300

    ajustes

--- src/app/core/dialog/dialog.component.ts
+++ src/app/core/dialog/dialog.component.ts
@@ -5,7 +5,7 @@ import { FormGroup, FormBuilder } from "@angular/forms";
 @Component({
   selector: "app-dialog",
   templateUrl: "./dialog.component.html",
-  styleUrls: ["./dialog.component.css"]
+  styleUrls: ["./dialog.component.scss"]
 })
 export class DialogComponent implements OnInit {
   public form: FormGroup;
